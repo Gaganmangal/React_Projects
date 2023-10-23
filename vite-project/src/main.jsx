8f6b9c36d0877da5cc0e459671117cc4abf747bf
Gaganmangal <mangalgagan4@gmail.com>
2023-10-23 19:02:28 +0530

    ...

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return(
+//     <h1>Gagan Mangal</h1>
+//   )
+// }
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com/",
+//     target: "_black",
+//   },
+//   children: "click to visit google",
+// };
+
+// const anotherreact = (
+//   <a href="https://www.google.com/" target='_black'>Click</a>
+// )
+
+const react = React.createElement(
+  'a',
+  {href:'https://www.google.com/',target: '_blank'},
+  'click me'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    
+    // MyApp()
+// anotherreact
+  react
 )
