938805c09012f684dbbce9494be59f0329590514
Gaganmangal <mangalgagan4@gmail.com>
2023-10-27 13:41:56 +0530

    ...

--- vite-project-04/src/App.jsx
+++ vite-project-04/src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [password, setpassword] = useState("");
+  const [length, setlength] = useState(8);
+  const [number, setnumber] = useState(false);
+  const [char, setchar] = useState(false);
 
-  return <></>;
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (number) string += "0123456789";
+    if (char) string += "!#$%&'()*+,-./:;<>=?@[]{}|^_~...∞©®™€¥£¢µ°²×÷§±∑";
+
+    for (let i = 1; i <= length; i++) {
+      pass += string;
+    }
+  }, [length, number, char, setpassword]);
+
+  return (
+    <>
+      <h1 className="text-center text-4xl text-white">Password Generator</h1>
+    </>
+  );
 }
 
 export default App;
