ed19877e46455dc55658b32b97f67526a1db9591
Gaganmangal <mangalgagan4@gmail.com>
2023-10-28 18:01:59 +0530

    ...

--- vite-project-05/src/Hooks/useCurrencyInfo.js
+++ vite-project-05/src/Hooks/useCurrencyInfo.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
   useEffect(() => {
-    const [data, setdata] = useState({});
     fetch(
-      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json"
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
-      .then((res) => res.json)
-      .then((res) => setdata(res[currency]));
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
   console.log(data);
