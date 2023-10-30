bf50dd6fbf8edc1ffbbadcb3208b6b1c59bd84d5
Gaganmangal <mangalgagan4@gmail.com>
2023-10-30 13:52:24 +0530

    ...

---
+++ vite-project-07/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" bg-purple-500 text-center text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
