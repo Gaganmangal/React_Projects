60afe12b2a45753f43b5fbd3f09166fcad6c6169
Gaganmangal <mangalgagan4@gmail.com>
2023-11-27 16:07:58 +0530

    ...

--- vite-project-09/src/App.jsx
+++ vite-project-09/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Heading from "./Components/Heading/Heading";
+import Pokedex from "./components/pokedex/pokedex";
 function App() {
   return (
     <>
-      <h1>Gagan</h1>
+      <Heading />
     </>
   );
 }
---
+++ vite-project-09/src/Components/Heading/Heading.jsx
@@ -0,0 +1,10 @@
+import "./Heading.css";
+export default function Heading() {
+  return (
+    <>
+      <h1 className="heading">
+        p<i></i>kédex
+      </h1>
+    </>
+  );
+}
