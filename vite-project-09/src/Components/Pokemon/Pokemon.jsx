60ea3db7ee506e101867c72d7205251d071a6e60
Gaganmangal <mangalgagan4@gmail.com>
2023-11-27 18:27:19 +0530

    ...

---
+++ vite-project-09/src/Components/Pokemon/Pokemon.jsx
@@ -0,0 +1,12 @@
+export default function Pokemon({ name, imge, type }) {
+  return (
+    <div>
+      {/* <span>{key}</span> */}
+      <h1>{name}</h1>
+      <img
+        src="https://images.pexels.com/photos/18788669/pexels-photo-18788669/free-photo-of-freckled-boy-with-head-rested-on-his-hand-sitting-next-to-his-little-brother-and-father-on-a-bench.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt=""
+      />
+    </div>
+  );
+}
--- vite-project-09/src/Components/PokemonList/PokemonList.jsx
+++ vite-project-09/src/Components/PokemonList/PokemonList.jsx
@@ -1,14 +1,43 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Pokemon from "../Pokemon/Pokemon";
 export default function PokemonList() {
   const [pokemonlist, setpokemonlist] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
-  async function downlodelist() {
+  async function downloadlist() {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    console.log(response.data.results);
+    const pokemonResult = response.data.results;
+    console.log(response.data);
+    const pokemonResultPromise = pokemonResult.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    const pokemondata = await axios.all(pokemonResultPromise);
+    console.log(pokemondata);
+    const res = pokemondata.map((datas) => {
+      const pokemon = datas.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other.dream_world.front_default,
+        type: pokemon.types,
+      };
+    });
+    console.log(res);
+    setpokemonlist(res);
+    setisLoading(false);
   }
   useEffect(() => {
-    downlodelist();
+    downloadlist();
   }, []);
-  return <>shdd</>;
+
+  return (
+    <div>
+      {isLoading
+        ? "Loading...."
+        : pokemonlist.map((p) => (
+            <Pokemon name={p.name} imge={p.image} key={p.id} />
+          ))}
+    </div>
+  );
 }
