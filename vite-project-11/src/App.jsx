5562f265c48a66bc5e07998930c12b033cfcd1e0
Gaganmangal <mangalgagan4@gmail.com>
2023-12-11 21:41:24 +0530

    ...

--- vite-project-11/src/App.jsx
+++ vite-project-11/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
