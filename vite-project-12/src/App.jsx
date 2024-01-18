c88265a92f69151890998ee94c20859b2e04ca03
Gaganmangal <mangalgagan4@gmail.com>
2024-01-18 20:33:49 +0530

    ...

--- vite-project-12/src/App.jsx
+++ vite-project-12/src/App.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import { FaPlusCircle } from "react-icons/fa";
+import Navbar from "./components/Navbar";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactRef = collection(db, "contact");
+        const contactSnapshort = await getDocs(contactRef);
+        console.log(contactRef);
+        const contactlist = contactSnapshort.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactlist);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getContacts();
+  }, []);
+  return (
+    <div className="mx-auto max-w-[370px]">
+      <Navbar />
+      <div className=" flex gap-2">
+        <div className=" relative flex flex-grow items-center ml-5">
+          <FiSearch className=" absolute ml-1 text-3xl text-white " />
+          <input
+            type="text"
+            className=" h-10 flex-grow rounded-md border bg-transparent border-white pl-9 text-white"
+          />
+        </div>
+        <FaPlusCircle className=" cursor-pointer text-5xl text-white" />
+      </div>
+    </div>
+  );
+};
 
-export default function App() {
-  return <div className="bg-red-500">App</div>;
-}
+export default App;
---
+++ vite-project-12/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="m-4 flex h-[60px] items-center justify-center gap-2 bg-white rounded-lg  text-xl font-medium">
+      <img src="/firebase.svg" alt="" />
+      <h1>Firebase Contact App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
