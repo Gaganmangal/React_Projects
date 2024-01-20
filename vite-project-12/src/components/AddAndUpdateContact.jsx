a764bb8899ef973f7ded889332bbce843bf23c72
Gaganmangal <mangalgagan4@gmail.com>
2024-01-20 17:59:29 +0530

    ...

--- vite-project-12/src/components/AddAndUpdateContact.jsx
+++ vite-project-12/src/components/AddAndUpdateContact.jsx
@@ -1,9 +1,15 @@
+import { Form } from "formik";
 import { Modal } from "./Modal";
+import { Formik } from
 export const AddAndUpdateContact = ({ isOpen, onClose }) => {
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        Model dsfsd
+        <Formik>
+            <Form>
+
+            </Form>
+        </Formik>
       </Modal>
     </div>
   );
